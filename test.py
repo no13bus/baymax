bd267007e3dbfb63fee80706ca9e174817b0e045
jqh <jqh@jqh-virtual-machine.(none)>
2014-12-09 16:39:43 +0800

    random

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ c.authorize_url
 auth_url = 'https://api.weibo.com/oauth2/authorize?redirect_uri=https%3A%2F%2Fapi.weibo.com%2Foauth2%2Fdefault.html&client_id=982519480'
 r = requests.get(auth_url)
 print r.url
+
 # https://api.weibo.com/oauth2/default.html?code=e3ff24403bf4f1d570a00cffd0b39ac8
 c.set_code('3ae8ff636f7ca71ef2cda13ac5ca9a7e')
 d = c.get('statuses/user_timeline', uid=2586104751, count=50)
